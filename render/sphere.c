1500b41b731e19fc77a0f7257774f012b920da75
Imran Mustafa <imustafa@lab1r1s8.42abudhabi.ae>
2022-11-15 15:39:27 +0400

    removed additional parameter from local normal plane

--- render/sphere.c
+++ render/sphere.c
@@ -59,7 +59,7 @@ t_vector	normal_at(t_shape s, t_point p)
 	if (!ft_strncmp(s.shape_name, "sp", 2))
 		obj_normal = local_normal_at_sphere(obj_point, point);
 	if (!ft_strncmp(s.shape_name, "pl", 2))
-		obj_normal = local_normal_at_plane(obj_point, point);
+		obj_normal = local_normal_at_plane();
 	if (!ft_strncmp(s.shape_name, "cy", 2))
 		obj_normal = local_normal_at_cylinder(s.shape,obj_point);	
 	tp3 = vector_tp(obj_normal);
